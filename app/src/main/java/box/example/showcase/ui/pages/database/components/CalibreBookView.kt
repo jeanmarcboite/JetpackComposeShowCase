c033786c48727ff3331b0821f79fd831470add46
jeanmarcboite <jeanmarc.boite@gmail.com>
2022-11-20 18:49:36 +0100

    htmltext

--- app/src/main/java/box/example/showcase/ui/pages/database/components/CalibreBookView.kt
+++ app/src/main/java/box/example/showcase/ui/pages/database/components/CalibreBookView.kt
@@ -1,24 +1,24 @@
 package box.example.showcase.ui.pages.database.components
 
-import android.widget.TextView
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.viewinterop.AndroidView
-import androidx.core.text.HtmlCompat
 import box.example.showcase.applib.books.models.calibre.CalibreBook
 import box.example.showcase.ui.components.OutlinedCard
 import box.example.showcase.ui.pages.database.LanguageMap
 import box.example.showcase.ui.theme.touchpoint_lg
 import coil.compose.rememberAsyncImagePainter
+import com.ireward.htmlcompose.HtmlText
 import com.jsramraj.flags.Flags
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -60,17 +60,15 @@ fun CalibreBook.View() {
                         .padding(8.dp)
                 )
                 comment?.apply {
-                    HtmlText(this)
+                    HtmlText(
+                        text = this,
+                        style = TextStyle(
+                            color = MaterialTheme.colorScheme.onPrimaryContainer,
+                            fontSize = MaterialTheme.typography.labelMedium.fontSize
+                        )
+                    )
                 }
             }
         }
     }
-}
-
-@Composable
-fun HtmlText(html: String, modifier: Modifier = Modifier) {
-    AndroidView(
-        modifier = modifier,
-        factory = { context -> TextView(context) }
-    ) { it.text = HtmlCompat.fromHtml(html, HtmlCompat.FROM_HTML_MODE_COMPACT) }
 }
