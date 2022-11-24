fccf10c5e85dbcade7cfcc33d9e73da152611a19
jeanmarcboite <jeanmarc.boite@gmail.com>
2022-11-24 11:12:32 +0100

    sort

--- app/src/main/java/box/example/showcase/applib/books/models/calibre/CalibreAuthor.kt
+++ app/src/main/java/box/example/showcase/applib/books/models/calibre/CalibreAuthor.kt
@@ -8,10 +8,5 @@ data class CalibreAuthor(
     @DatabaseField
     val link: String? = null,
     val books: MutableList<CalibreBook> = mutableListOf(),
-) : CalibreSortableEntity() {
-    override fun equals(other: Any?): Boolean {
-        return other != null
-                && other.javaClass == javaClass
-                && id == (other as CalibreEntity).id
-    }
-}
+) : CalibreSortableEntity()
+
--- app/src/main/java/box/example/showcase/ui/pages/database/components/CalibreAuthorView.kt
+++ app/src/main/java/box/example/showcase/ui/pages/database/components/CalibreAuthorView.kt
@@ -3,8 +3,6 @@ package box.example.showcase.ui.pages.database.components
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Card
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -13,7 +11,6 @@ import box.example.showcase.applib.books.models.calibre.CalibreAuthor
 import box.example.showcase.ui.components.OutlinedCard
 
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CalibreAuthor.View() {
         OutlinedCard(
